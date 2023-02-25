09d29742be897f80393246e66cd02c3016776087
Aytacrustemli <aytacrustmli93@gmail.com>
2023-02-25 18:41:35 +0400

    contact page

--- js/main.js
+++ js/main.js
@@ -17,3 +17,23 @@ $("#slide-testimonal").owlCarousel({
   },
 });
 
+const sendEmailBtn = document.getElementById("send-email");
+const emailAddress = "aytacrustmli93@gmail.com";
+
+sendEmailBtn.addEventListener("click", () => {
+  const messageInput = document.getElementById("message-input");
+  const messageBody = messageInput.value;
+  const mailtoLink = `mailto:${emailAddress}?body=${encodeURIComponent(messageBody)}`;
+  window.location.href = mailtoLink;
+});
+
+
+function initMap() {
+  const map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 40.7128, lng: -74.0060 },
+    zoom: 8,
+  });
+  
+}
+
+google.maps.event.addDomListener(window, 'load', initMap);
